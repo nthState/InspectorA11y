dc8977554e59924597eb46d38786356cba208a84
Chris Davis <chris@nthstate.com>
2024-09-13 11:46:21 +0100

    chore: Using the Mirror API to see if we can find the modifiers

--- Sources/InspectorA11yCore/TestView.swift
+++ Sources/InspectorA11yCore/TestView.swift
@@ -15,22 +15,24 @@ extension TestView: View {
       Rectangle().fill(Color(red: 4/255, green: 5/255, blue: 15/255).gradient)
       VStack {
         Image(systemName: "photo")
-          .instruction(id: "2", "A photo", order: 2)
           .font(.system(size: 80))
           .background(in: Circle().inset(by: -40))
           .backgroundStyle(.blue.gradient)
           .foregroundStyle(.white.shadow(.drop(radius: 1, y: 1.5)))
           .padding(60)
+          .instruction(id: "2", "A photo", order: 2)
         Text("Hello, world!")
-          .instruction(id: "1", "say this", order: 1)
           .foregroundStyle(Color.orange)
           .font(.largeTitle)
-          .accessibility(sortPriority: 20)
+          .accessibilitySortPriority(123)              // not found
+          .accessibility(sortPriority: 20)            // AccessibilityPropertiesEntry AccessibilityAttachmentModifier
+          .accessibilityLabel("some label")           // AccessibilityLabelStorage AccessibilityAttachmentModifier
+          .instruction(id: "1", "say this", order: 1) // ActionInstructionModifier
         Button(action: {}, label: {
           Text("Button")
         })
-        .instruction(id: "3", "button", order: 1000)
         .accessibility(sortPriority: 1000)
+        .instruction(id: "3", "button", order: 1000)
       }
     }
     .frame(height: 400)
--- Tests/InspectorA11yCoreTests/InspectorA11yCoreTests.swift
+++ Tests/InspectorA11yCoreTests/InspectorA11yCoreTests.swift
@@ -2,6 +2,7 @@
 //  Copyright © nthState Ltd. 2024. All rights reserved.
 //
 
+import SwiftUI
 import XCTest
 @testable import InspectorA11yCore
 
@@ -30,5 +31,30 @@ final class InspectorA11yCoreTests: XCTestCase {
 
     XCTAssertNotNil(image)
   }
+
+  func testMirror() {
+
+    let item = TestView().body
+      .environment(TestObservable())
+
+    inspect(item: item, indent: 0)
+  }
+
+  func inspect(item: Any, indent: Int) {
+    let mirror = Mirror(reflecting: item)
+    let children = Array(mirror.children)
+    print(mirror)
+
+    for child in children {
+      print("\(String(repeating: " ", count: indent))child: \(child.label) \(child.value)")
+
+      inspect(item: child.value, indent: indent + 1)
+    }
+  }
+
+}
+
+@Observable
+class TestObservable {
 
 }
